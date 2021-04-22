4fedec5bc264c4b3bc2f7e1169f9bdf8c3ea6e8a
gaoguanqi <307590625@qq.com>
2021-04-22 23:26:43 +0800

    core

---
+++ service-core/src/main/java/com/atguigu/srb/core/mapper/LendMapper.java
@@ -0,0 +1,16 @@
+package com.atguigu.srb.core.mapper;
+
+import com.atguigu.srb.core.pojo.entity.Lend;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 标的准备表 Mapper 接口
+ * </p>
+ *
+ * @author gaoguanqi
+ * @since 2021-04-22
+ */
+public interface LendMapper extends BaseMapper<Lend> {
+
+}
---
+++ service-core/src/main/java/com/atguigu/srb/core/mapper/UserBindMapper.java
@@ -0,0 +1,16 @@
+package com.atguigu.srb.core.mapper;
+
+import com.atguigu.srb.core.pojo.entity.UserBind;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 用户绑定表 Mapper 接口
+ * </p>
+ *
+ * @author gaoguanqi
+ * @since 2021-04-22
+ */
+public interface UserBindMapper extends BaseMapper<UserBind> {
+
+}
