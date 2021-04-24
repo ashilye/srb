c83a15142a3991b4aca7ba223f8e538e2101e6ea
gaoguanqi <307590625@qq.com>
2021-04-24 16:58:21 +0800

    R

--- service-core/src/main/java/com/atguigu/srb/core/controller/admin/AdminIntegralGradeController.java
+++ service-core/src/main/java/com/atguigu/srb/core/controller/admin/AdminIntegralGradeController.java
@@ -1,12 +1,19 @@
 package com.atguigu.srb.core.controller.admin;
 
 
+import ch.qos.logback.core.joran.util.beans.BeanUtil;
+import com.atguigu.common.result.R;
+import com.atguigu.srb.core.pojo.entity.IntegralGrade;
 import com.atguigu.srb.core.service.IntegralGradeService;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.baomidou.mybatisplus.core.toolkit.BeanUtils;
+import com.baomidou.mybatisplus.core.toolkit.CollectionUtils;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * <p>
@@ -16,6 +23,7 @@ import javax.annotation.Resource;
  * @author gaoguanqi
  * @since 2021-04-22
  */
+@Api(tags = "积分等级管理")
 @CrossOrigin  //跨域
 @RestController
 @RequestMapping("/admin/core/integralGrade")
@@ -24,6 +32,65 @@ public class AdminIntegralGradeController {
     @Resource
     private IntegralGradeService integralGradeService;
 
+
+    @ApiOperation(value = "积分等级列表")
+    @GetMapping("/list")
+    public R listAll(){
+        List<IntegralGrade> list = integralGradeService.list();
+        return R.ok().data("list",list).message("获取列表成功");
+    }
+
+    @ApiOperation(value = "根据id删除数据记录",notes = "逻辑删除数据记录")
+    @DeleteMapping("/remove/{id}")
+    public R removeById(
+            @ApiParam(value = "数据id", example = "1",required = true)
+            @PathVariable Long id){
+        boolean result = integralGradeService.removeById(id);
+        if(result){
+            return R.ok().message("删除成功");
+        }else {
+            return R.error().message("删除失败");
+        }
+    }
+
+    @ApiOperation(value = "新增积分等级")
+    @PostMapping("/save")
+    public R save(
+            @ApiParam(value = "积分等级对象",required = true)
+            @RequestBody IntegralGrade integralGrade){
+        boolean result = integralGradeService.save(integralGrade);
+        if(result){
+            return R.ok().message("保存成功");
+        }else {
+            return R.error().message("保存失败");
+        }
+    }
+
+    @ApiOperation(value = "根据id获取积分等级")
+    @GetMapping("/get/{id}")
+    public R getById(
+            @ApiParam(value = "数据id",required = true,example = "1")
+            @PathVariable Long id){
+        IntegralGrade integralGrade = integralGradeService.getById(id);
+        if(integralGrade != null){
+            return R.ok().data("record",integralGrade);
+        }else {
+            return R.error().message("数据获取失败");
+        }
+    }
+
+    @ApiOperation(value = "更新积分等级")
+    @PutMapping("/update")
+    public R updateById(
+            @ApiParam(value = "积分等级对象",required = true)
+            @RequestBody IntegralGrade integralGrade){
+        boolean result = integralGradeService.updateById(integralGrade);
+        if(result){
+            return R.ok().message("更新成功");
+        }else {
+            return R.error().message("更新失败");
+        }
+    }
 
 }
 
